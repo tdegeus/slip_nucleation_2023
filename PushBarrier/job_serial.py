13bd7877bf4518fdf33d6e0c7d89368bb421cf26
Tom de Geus <tom@geus.me>
2020-12-24 15:21:49 +0100

    Fixing typo

--- PushBarrier/job_serial.py
+++ PushBarrier/job_serial.py
@@ -33,7 +33,7 @@ for file in files:
 
     basename = os.path.splitext(file)[0]
 
-    command = 'PushBarrier "{0:s}"" "{1:s}"'.format(file, basename)
+    command = 'PushBarrier "{0:s}" "{1:s}"'.format(file, basename)
     command = slurm.format(command)
 
     sbatch = {
