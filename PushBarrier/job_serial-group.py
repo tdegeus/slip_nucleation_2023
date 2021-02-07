c6a87a8dcdff0153ff6fc6c70cd6e654d5b0b4d0
Tom de Geus <tom@geus.me>
2021-02-07 17:27:44 +0100

    [PushBarrier] Updating job-scripts

--- PushBarrier/job_serial-group.py
+++ PushBarrier/job_serial-group.py
@@ -5,11 +5,14 @@ import re
 import subprocess
 import h5py
 import GooseSLURM as gs
+import numpy as np
 
 # ----
 
 files = sorted(list(filter(None, subprocess.check_output(
     "find . -iname 'id*.hdf5'", shell=True).decode('utf-8').split('\n'))))
+files = [str(i) + '.hdf5' for i in range(223)]
+
 
 # ----
 
@@ -24,21 +27,33 @@ if [[ "${{SYS_TYPE}}" == *E5v4* ]]; then
     conda activate code_velocity_E5v4
 elif [[ "${{SYS_TYPE}}" == *s6g1* ]]; then
     conda activate code_velocity_s6g1
+else
+    echo "Unknown SYS_TYPE ${{SYS_TYPE}}"
 fi
 
 {0:s}
 '''
 
-for file in files:
+files_per_group = 10
+print(files)
 
-    basename = os.path.splitext(os.path.normpath(file))[0]
+for group in range(int(np.ceil(len(files) / files_per_group))):
 
-    command = 'PushBarrier "{0:s}" "{1:s}"'.format(file, basename)
+    f = files[group * files_per_group: (group + 1) * files_per_group]
+    c = []
+
+    for file in f:
+        basename = os.path.splitext(os.path.normpath(file))[0]
+        c += ['PushBarrier "{0:s}" "{1:s}"'.format(file, basename)]
+
+    command = '\n'.join(c)
     command = slurm.format(command)
 
+    jobname = 'PushBarrier-{0:d}'.format(group)
+
     sbatch = {
-        'job-name': basename,
-        'out': basename + '.out',
+        'job-name': jobname,
+        'out': jobname + '.out',
         'nodes': 1,
         'ntasks': 1,
         'cpus-per-task': 1,
@@ -48,4 +63,4 @@ for file in files:
         'mem' : '8G',
     }
 
-    open(basename + '.slurm', 'w').write(gs.scripts.plain(command=command, **sbatch))
+    open(jobname + '.slurm', 'w').write(gs.scripts.plain(command=command, **sbatch))
