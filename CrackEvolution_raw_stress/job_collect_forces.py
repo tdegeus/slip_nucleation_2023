fdde0f6943dfe9c400b83c61b12d0ecfae5a8b79
Tom de Geus <tom@geus.me>
2021-02-23 22:51:46 +0100

    Changing env

--- CrackEvolution_raw_stress/job_collect_forces.py
+++ CrackEvolution_raw_stress/job_collect_forces.py
@@ -8,24 +8,20 @@ export OMP_NUM_THREADS=1
 # load conda environment
 source ~/miniconda3/etc/profile.d/conda.sh
 
-if [[ "${{SYS_TYPE}}" == *E5v4* ]]; then
-    conda activate code_velocity_E5v4
-elif [[ "${{SYS_TYPE}}" == *s6g1* ]]; then
-    conda activate code_velocity_s6g1
-fi
+conda activate code_collect_s6g1
 
 {command:s}
 '''
 
 fbase = "job_collect_forces"
 cmd = [
-    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=0d6 list_stress=0d6.yaml",
-    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=1d6 list_stress=1d6.yaml",
-    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=2d6 list_stress=2d6.yaml",
-    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=3d6 list_stress=3d6.yaml",
-    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=4d6 list_stress=4d6.yaml",
-    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=5d6 list_stress=5d6.yaml",
-    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=6d6 list_stress=6d6.yaml"]
+    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=0d6.hdf5 list_stress=0d6.yaml",
+    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=1d6.hdf5 list_stress=1d6.yaml",
+    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=2d6.hdf5 list_stress=2d6.yaml",
+    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=3d6.hdf5 list_stress=3d6.yaml",
+    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=4d6.hdf5 list_stress=4d6.yaml",
+    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=5d6.hdf5 list_stress=5d6.yaml",
+    "python ../collect_forces.py --force -i ../../../data/nx=3\^6x2/EnsembleInfo.hdf5 -o out_stress=6d6.hdf5 list_stress=6d6.yaml"]
 
 for i, c in enumerate(cmd):
 
