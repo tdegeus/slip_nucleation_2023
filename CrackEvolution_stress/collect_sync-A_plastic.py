Fixing normalization

CrackEvolution_stress/collect_sync-A_plastic.py
@@ -461,32 +461,45 @@ with h5py.File(output, 'w') as data:
             red[key][field] = np.array(out[key][field], copy=True)
 
     # remove data outside crack
+    n = np.array(A, copy=True)
     mid = int((nx - nx % 2) / 2)
-    il = int(mid - 100)
-    iu = int(mid + 100)
-    for key in red:
-        for field in red[key]:
-            red[key][field] = red[key][field].astype(np.float64)
-            red[key][field][:, :il] = 0.
-            red[key][field][:, iu:] = 0.
+
+    for i, a in enumerate(A):
+
+        il = int(mid - a / 2)
+        iu = int(mid + a / 2)
+        if iu - il > 200:
+            il = int(mid - 100)
+            iu = int(mid + 100)
+
+        n[i] = iu - il
+
+        for key in red:
+            for field in red[key]:
+                red[key][field] = red[key][field].astype(np.float64)
+                red[key][field][i, :il] = 0.
+                red[key][field][i, iu:] = 0.
+
+    n[n == 0] = 1
+    n = n.astype(np.float64)
 
     # compute mean
-    m_sig_xx  = compute_mean(np.sum(red['1st']['sig_xx' ], axis=1), norm * 200)
-    m_sig_xy  = compute_mean(np.sum(red['1st']['sig_xy' ], axis=1), norm * 200)
-    m_sig_yy  = compute_mean(np.sum(red['1st']['sig_yy' ], axis=1), norm * 200)
-    m_S       = compute_mean(np.sum(red['1st']['S'      ], axis=1), norm * 200)
-    m_epsp    = compute_mean(np.sum(red['1st']['epsp'   ], axis=1), norm * 200)
-    m_epspdot = compute_mean(np.sum(red['1st']['epspdot'], axis=1), norm * 200)
-    m_moved   = compute_mean(np.sum(red['1st']['moved'  ], axis=1), norm * 200)
+    m_sig_xx  = compute_mean(np.sum(red['1st']['sig_xx' ], axis=1), norm * n)
+    m_sig_xy  = compute_mean(np.sum(red['1st']['sig_xy' ], axis=1), norm * n)
+    m_sig_yy  = compute_mean(np.sum(red['1st']['sig_yy' ], axis=1), norm * n)
+    m_S       = compute_mean(np.sum(red['1st']['S'      ], axis=1), norm * n)
+    m_epsp    = compute_mean(np.sum(red['1st']['epsp'   ], axis=1), norm * n)
+    m_epspdot = compute_mean(np.sum(red['1st']['epspdot'], axis=1), norm * n)
+    m_moved   = compute_mean(np.sum(red['1st']['moved'  ], axis=1), norm * n)
 
     # compute variance
-    v_sig_xx  = compute_variance(np.sum(red['1st']['sig_xx' ], axis=1), np.sum(red['2nd']['sig_xx' ], axis=1) , norm * 200)
-    v_sig_xy  = compute_variance(np.sum(red['1st']['sig_xy' ], axis=1), np.sum(red['2nd']['sig_xy' ], axis=1) , norm * 200)
-    v_sig_yy  = compute_variance(np.sum(red['1st']['sig_yy' ], axis=1), np.sum(red['2nd']['sig_yy' ], axis=1) , norm * 200)
-    v_S       = compute_variance(np.sum(red['1st']['S'      ], axis=1), np.sum(red['2nd']['S'      ], axis=1) , norm * 200)
-    v_epsp    = compute_variance(np.sum(red['1st']['epsp'   ], axis=1), np.sum(red['2nd']['epsp'   ], axis=1) , norm * 200)
-    v_epspdot = compute_variance(np.sum(red['1st']['epspdot'], axis=1), np.sum(red['2nd']['epspdot'], axis=1) , norm * 200)
-    v_moved   = compute_variance(np.sum(red['1st']['moved'  ], axis=1), np.sum(red['2nd']['moved'  ], axis=1) , norm * 200)
+    v_sig_xx  = compute_variance(np.sum(red['1st']['sig_xx' ], axis=1), np.sum(red['2nd']['sig_xx' ], axis=1) , norm * n)
+    v_sig_xy  = compute_variance(np.sum(red['1st']['sig_xy' ], axis=1), np.sum(red['2nd']['sig_xy' ], axis=1) , norm * n)
+    v_sig_yy  = compute_variance(np.sum(red['1st']['sig_yy' ], axis=1), np.sum(red['2nd']['sig_yy' ], axis=1) , norm * n)
+    v_S       = compute_variance(np.sum(red['1st']['S'      ], axis=1), np.sum(red['2nd']['S'      ], axis=1) , norm * n)
+    v_epsp    = compute_variance(np.sum(red['1st']['epsp'   ], axis=1), np.sum(red['2nd']['epsp'   ], axis=1) , norm * n)
+    v_epspdot = compute_variance(np.sum(red['1st']['epspdot'], axis=1), np.sum(red['2nd']['epspdot'], axis=1) , norm * n)
+    v_moved   = compute_variance(np.sum(red['1st']['moved'  ], axis=1), np.sum(red['2nd']['moved'  ], axis=1) , norm * n)
 
     # store
     store(data, 'center',

CrackEvolution_stress/collect_sync-t(A=N)_plastic.py
@@ -84,6 +84,7 @@ plas_epsp = np.zeros(plas_shape, dtype='float')
 plas_epspdot = np.zeros(plas_shape, dtype='float')
 
 pbar = tqdm.tqdm(files)
+nfiles = 0
 
 dstep = 10 # with storage every 500 increments this corresponds roughly to dA = 50 during nucleation
 astep = 50
@@ -113,6 +114,8 @@ for ifile, file in enumerate(pbar):
 
         if "/sync-t/stored" not in data:
             continue
+
+        nfiles += 1
 
         A = data["/sync-A/stored"][...]
         T = data["/sync-t/global/iiter"][...]
@@ -205,7 +208,8 @@ plas_epspdot = plas_epspdot[:, :imax, :] / eps0 / (dt / t0)
 with h5py.File(output, 'w') as data:
 
     data['/meta/versions/{0:s}'.format(os.path.basename(__file__))] = get_version(root='..', relative_to=__file__)
-    data['/glob_norm'] = np.mean(glob_norm, axis=0)
+    data['/glob_norm'] = np.sum(glob_norm, axis=0) / float(nfiles)
+    data['/glob_norm'].attrs['nfiles'] = nfiles
     data['/plastic/r/sig_xx'] = np.average(plas_sig_xx, weights=plas_norm, axis=0)
     data['/plastic/r/sig_xy'] = np.average(plas_sig_xy, weights=plas_norm, axis=0)
     data['/plastic/r/sig_yy'] = np.average(plas_sig_yy, weights=plas_norm, axis=0)