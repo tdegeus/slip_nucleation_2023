47daf643d94a81ccf6c6b94c1791e06b1c439016
Tom de Geus <tom@geus.me>
2021-09-01 17:56:41 +0200

    Finishing

--- Run/PinAndTrigger_rerun_sync-A.py
+++ Run/PinAndTrigger_rerun_sync-A.py
@@ -1,15 +1,19 @@
+import argparse
 import os
+import re
 import sys
 
 import FrictionQPotFEM.UniformSingleLayer2d as model
 import numpy as np
+import h5py
+import yaml
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 import PinAndTrigger  # noqa: E402
 
 
 def run_dynamics(
-    system: model.System, target_element: int, target_A: int, eps_kick: float
+    system: model.System, target_element: int, target_A: int, eps_kick: float, sig0: float, t0: float
 ) -> dict:
     """
     Run the dynamics of an event, saving the state at the interface at every "A".
@@ -23,6 +27,12 @@ def run_dynamics(
 
     :param target_A:
         Number of blocks to keep unpinned (``target_A / 2`` on both sides of ``target_element``).
+
+    :param sig0
+        Stress normalisation.
+
+    :param t0
+        Time normalisation.
 
     :param eps_kick:
         Strain kick to use.
@@ -42,8 +52,8 @@ def run_dynamics(
     N = plastic.size
     dV = system.quad().AsTensor(2, system.quad().dV())
     plastic_dV = dV[plastic, ...]
-    idx_n = system.plastic_CurrentIndex()[:, 0].astype(int)
     pinned = PinAndTrigger.pinsystem(system, target_element, target_A)
+    idx_n = system.plastic_CurrentIndex()[:, 0].astype(int)
     system.triggerElementWithLocalSimpleShear(eps_kick, target_element)
 
     a_n = 0
@@ -72,14 +82,14 @@ def run_dynamics(
             plastic_sig = np.average(system.plastic_Sig(), weights=plastic_dV, axis=1)
 
             # store to output (broadcast if needed)
-            ret["sig_xx"][a_n:a, :] = plastic_sig[:, 0, 0].reshape(1, -1)
-            ret["sig_xy"][a_n:a, :] = plastic_sig[:, 0, 1].reshape(1, -1)
-            ret["sig_yy"][a_n:a, :] = plastic_sig[:, 1, 1].reshape(1, -1)
+            ret["sig_xx"][a_n:a, :] = plastic_sig[:, 0, 0].reshape(1, -1) / sig0
+            ret["sig_xy"][a_n:a, :] = plastic_sig[:, 0, 1].reshape(1, -1) / sig0
+            ret["sig_yy"][a_n:a, :] = plastic_sig[:, 1, 1].reshape(1, -1) / sig0
             ret["idx"][a_n:a, :] = idx.reshape(1, -1)
-            ret["t"][a_n:a] = system.t()
-            ret["Sig_xx"][a_n:a] = sig[0, 0]
-            ret["Sig_xy"][a_n:a] = sig[0, 1]
-            ret["Sig_yy"][a_n:a] = sig[1, 1]
+            ret["t"][a_n:a] = system.t() / t0
+            ret["Sig_xx"][a_n:a] = sig[0, 0] / sig0
+            ret["Sig_xy"][a_n:a] = sig[0, 1] / sig0
+            ret["Sig_yy"][a_n:a] = sig[1, 1] / sig0
 
         if a >= target_A:
             break
@@ -90,3 +100,52 @@ def run_dynamics(
     ret["idx"] = ret["idx"][:, np.logical_not(pinned)]
 
     return ret
+
+
+if __name__ == "__main__":
+
+    basename = os.path.splitext(os.path.basename(__file__))[0]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", type=str, help="Configuration file")
+    args = parser.parse_args()
+
+    assert os.path.isfile(os.path.realpath(args.file))
+
+    with open(args.file) as file:
+        config = yaml.load(file.read(), Loader=yaml.FullLoader)
+
+    assert os.path.isfile(os.path.realpath(config["info"]))
+
+    with h5py.File(config["info"], "r") as data:
+        sig0 = data["/normalisation/sig0"][...]
+        t0 = data["/normalisation/t0"][...]
+
+    system = None
+
+    with h5py.File(config["output"], "w") as output:
+
+        with h5py.File(config["collected"], "r") as data:
+
+            for path in config["paths"]:
+
+                print(path)
+
+                origsim = str(data["data"][path]["file"].asstr()[...])
+                e = int(re.split(r"(element=)([0-9]*)", path)[2])
+                a = int(re.split(r"(A=)([0-9]*)", path)[2])
+
+                with h5py.File(origsim, "r") as mysim:
+                    if system is None:
+                        system = PinAndTrigger.initsystem(mysim)
+                        eps_kick = mysim["/run/epsd/kick"][...]
+                    else:
+                        PinAndTrigger.reset_epsy(system, mysim)
+
+                system.setU(data["data"][path]["disp"]["0"][...])
+
+                ret = run_dynamics(system, e, a, eps_kick, sig0, t0)
+
+                for key in ret:
+                    output[f"/data/{path}/{key}"] = ret[key]
+
