ba4f5ab7698c307b53e5583e35748179970cf44d
Tom de Geus <tom@geus.me>
2021-10-19 17:12:25 +0200

    Adding flow simulations

---
+++ tests/Flow.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+import sys
+import unittest
+import numpy as np
+import h5py
+
+root = os.path.join(os.path.dirname(__file__), "..")
+if os.path.exists(os.path.join(root, "mycode_front", "_version.py")):
+    sys.path.insert(0, os.path.abspath(root))
+
+import mycode_front as my  # noqa: E402
+
+
+class MyTests(unittest.TestCase):
+    """
+    Tests
+    """
+
+    def test_small(self):
+
+        dirname = "mytest"
+
+        if os.path.isdir(dirname):
+            shutil.rmtree(dirname)
+
+        os.makedirs(dirname)
+
+        N = 9
+        files = my.Flow.cli_generate(["-N", N, "--dev", dirname])
+
+        my.Flow.run(files[-1], dev=True, maxinc=int(1e7))
+
+        shutil.rmtree(dirname)
+
+
+
+
+
+if __name__ == "__main__":
+
+    unittest.main()
