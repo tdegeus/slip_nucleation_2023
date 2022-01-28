24a9b3b3222d953e1d63abdd54f7bf66eb90eccb
Tom de Geus <tom@geus.me>
2022-01-28 13:15:42 +0100

    Trigger: adding output (changes output in main function)

--- mycode_front/Trigger.py
+++ mycode_front/Trigger.py
@@ -10,8 +10,10 @@ import textwrap
 
 import click
 import FrictionQPotFEM.UniformSingleLayer2d as model
+import GooseHDF5 as g5
 import h5py
 import numpy as np
+import tqdm
 
 from . import slurm
 from . import System
@@ -27,7 +29,6 @@ entry_points = dict(
 file_defaults = dict(
     cli_ensembleinfo="Trigger_EnsembleInfo.h5",
 )
-
 
 
 def replace_ep(doc: str) -> str:
@@ -78,47 +79,48 @@ def cli_run(cli_args=None):
     assert os.path.realpath(args.input) != os.path.realpath(args.output)
     tools._check_overwrite_file(args.output, args.force)
 
-    seed = None
-    uuid = None
-
     print("starting:", args.output)
 
-    with h5py.File(args.input, "r") as file:
-
-        system = System.init(file)
-        eps_kick = file["/run/epsd/kick"][...]
-
-        if "uuid" in file:
-            uuid = str(file["/uuid"].asstr()[...])
-        else:
-            seed = file["/meta/seed_base"][...]
-
-        if args.inc is not None:
-
-            system.setU(file[f"/disp/{args.inc:d}"])
-
-        else:
-
-            # determine at which increment a push could be applied
-
-            inc_system, inc_push = System.pushincrements(system, file, args.stress)
-
-            # reload specific increment based on target stress and system-spanning increment
-
-            assert args.incc in inc_system
-            i = np.argmax(np.logical_and(args.incc == inc_system, args.incc <= inc_push))
-            inc = inc_push[i]
-            assert args.incc == inc_system[i]
-
-            system.setU(file[f"/disp/{inc:d}"])
-            idx_n = system.plastic_CurrentIndex()
-            system.addSimpleShearToFixedStress(args.stress)
-            idx = system.plastic_CurrentIndex()
-            assert np.all(idx == idx_n)
-
-    # (*) Apply push and minimise energy
-
     with h5py.File(args.output, "w") as output:
+
+        # (*) Initialise system
+
+        with h5py.File(args.input, "r") as file:
+
+            system = System.init(file)
+            eps_kick = file["/run/epsd/kick"][...]
+
+            copy = ["/run/dt"]
+            if "uuid" in file:
+                copy += ["/uuid"]
+            else:
+                copy += ["/meta/seed_base", "/meta/normalisation"]
+            g5.copy(file, output, copy)
+
+            if args.inc is not None:
+
+                system.setU(file[f"/disp/{args.inc:d}"])
+
+            else:
+
+                # determine at which increment a push could be applied
+
+                inc_system, inc_push = System.pushincrements(system, file, args.stress)
+
+                # reload specific increment based on target stress and system-spanning increment
+
+                assert args.incc in inc_system
+                i = np.argmax(np.logical_and(args.incc == inc_system, args.incc <= inc_push))
+                inc = inc_push[i]
+                assert args.incc == inc_system[i]
+
+                system.setU(file[f"/disp/{inc:d}"])
+                idx_n = system.plastic_CurrentIndex()
+                system.addSimpleShearToFixedStress(args.stress)
+                idx = system.plastic_CurrentIndex()
+                assert np.all(idx == idx_n)
+
+        # (*) Apply push and minimise energy
 
         output["/disp/0"] = system.u()
         idx_n = system.plastic_CurrentIndex()[:, 0].astype(int)
@@ -132,11 +134,15 @@ def cli_run(cli_args=None):
 
         if niter > 0:
             output["/disp/1"] = system.u()
+            output["/stored"] = [0, 1]
+            output["/kick"] = [False, True]
         else:
             output["/restart/u"] = system.u()
             output["/restart/v"] = system.v()
             output["/restart/a"] = system.a()
             output["/restart/t"] = system.t()
+            output["/stored"] = [0]
+            output["/kick"] = [False]
 
         idx = system.plastic_CurrentIndex()[:, 0].astype(int)
 
@@ -144,8 +150,10 @@ def cli_run(cli_args=None):
 
         meta = output.create_group(f"/meta/{progname}")
         meta.attrs["file"] = os.path.relpath(args.input, os.path.dirname(args.output))
+        meta.attrs["filepath"] = args.input
         meta.attrs["version"] = version
         meta.attrs["dependencies"] = System.dependencies(model)
+        meta.attrs["truncated"] = niter == 0
         meta.attrs["target_stress"] = args.stress
         meta.attrs["target_inc_system"] = args.incc
         meta.attrs["target_element"] = args.element
@@ -155,10 +163,99 @@ def cli_run(cli_args=None):
         meta.attrs["S"] = np.sum(idx - idx_n)
         meta.attrs["A"] = np.sum(idx != idx_n)
 
-        if seed is not None:
-            meta.attrs["seed_base"] = seed
-        else:
-            meta.attrs["uuid"] = uuid
+    if cli_args is not None:
+        return args.output
+
+
+def cli_ensembleinfo(cli_args=None):
+    """
+    Read basic info from pushes.
+    """
+
+    class MyFmt(
+        argparse.RawDescriptionHelpFormatter,
+        argparse.ArgumentDefaultsHelpFormatter,
+        argparse.MetavarTypeHelpFormatter,
+    ):
+        pass
+
+    funcname = inspect.getframeinfo(inspect.currentframe()).function
+    doc = textwrap.dedent(inspect.getdoc(globals()[funcname]))
+    parser = argparse.ArgumentParser(formatter_class=MyFmt, description=replace_ep(doc))
+    progname = entry_points[funcname]
+    output = file_defaults[funcname]
+
+    parser.add_argument("-f", "--force", action="store_true", help="Force overwrite output")
+    parser.add_argument("-o", "--output", type=str, default=output, help="Output file")
+    parser.add_argument("files", nargs="*", type=str, help="Files to read")
+
+    args = tools._parse(parser, cli_args)
+    assert len(args.files) > 0
+    assert all([os.path.isfile(file) for file in args.files])
+    tools._check_overwrite_file(args.output, args.force)
+
+    ret = dict(
+        S=[],
+        A=[],
+        xi=[],
+        epsd=[],
+        epsd0=[],
+        sigd=[],
+        sigd0=[],
+        truncated=[],
+        element=[],
+        inc_c=[],
+        file=[],
+        version=[],
+        dependency=[],
+    )
+
+    for i, filepath in enumerate(tqdm.tqdm(args.files)):
+
+        with h5py.File(filepath, "r") as file:
+
+            meta = file[f"/meta/{entry_points['cli_run']}"]
+            sourcepath = meta.attrs["filepath"]
+
+            with h5py.File(sourcepath, "r") as source:
+
+                if i == 0:
+                    system = System.init(source)
+                else:
+                    system.reset_epsy(System.read_epsy(source))
+
+            out = System.basic_output(system, file, verbose=False)
+            assert out["S"][1] == meta.attrs["S"]
+            assert out["A"][1] == meta.attrs["A"]
+
+            ret["S"].append(out["S"][1])
+            ret["A"].append(out["A"][1])
+            ret["xi"].append(out["xi"][1])
+            ret["epsd"].append(out["epsd"][1])
+            ret["epsd0"].append(out["epsd"][0])
+            ret["sigd"].append(out["epsd"][1])
+            ret["sigd0"].append(out["epsd"][0])
+            ret["truncated"].append(meta.attrs["truncated"])
+            ret["element"].append(meta.attrs["target_element"])
+            ret["inc_c"].append(meta.attrs["target_inc_system"])
+            ret["file"].append(meta.attrs["file"])
+            ret["version"].append(meta.attrs["version"])
+            ret["dependency"].append(";".join(meta.attrs["dependencies"]))
+
+    with h5py.File(args.output, "w") as output:
+
+        for key in ["file", "version", "dependency"]:
+            ret[f"{key}s"], ret[key] = np.unique(ret[key], return_inverse=True)
+            ret[f"{key}s"] = [str(i) for i in ret[f"{key}s"]]
+
+        ret["dependencies"] = [i.split(";") for i in ret.pop("dependencys")]
+
+        for key in ret:
+            output[key] = ret[key]
+
+        meta = output.create_group(f"/meta/{progname}")
+        meta.attrs["version"] = version
+        meta.attrs["dependencies"] = System.dependencies(model)
 
 
 def cli_job_strain(cli_args=None):
