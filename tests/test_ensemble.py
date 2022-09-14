ababcc0e159580ed92298d9d13d7612ffc0a5d41
Tom de Geus <tom@geus.me>
2022-09-14 15:21:11 +0200

    porting tests

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -5,7 +5,9 @@ import shutil
 import sys
 import unittest
 
+import enstat
 import GMatElastoPlasticQPot.Cartesian2d as GMat
+import GooseFEM
 import GooseHDF5 as g5
 import h5py
 import numpy as np
@@ -21,6 +23,7 @@ from mycode_front import EventMap  # noqa: E402
 from mycode_front import QuasiStatic  # noqa: E402
 from mycode_front import storage  # noqa: E402
 from mycode_front import Trigger  # noqa: E402
+from mycode_front import tools  # noqa: E402
 
 basedir = pathlib.Path(__file__).parent / "output"
 qsdir = basedir / "QuasiStatic"
@@ -296,7 +299,7 @@ class test_Trigger(unittest.TestCase):
 
             self.assertTrue(g5.allequal(source, dest, paths))
 
-    def test_rerun_dynamics(self):
+    def test_rerun_dynamics_ensemble(self):
         """
         Rerun one trigger with Dynamics, check the macroscopic stress.
         """
@@ -322,12 +325,17 @@ class test_Trigger(unittest.TestCase):
         _, _, outpath, _, step, _, _, inpath = commands[0].split(" ")
         step = int(step)
 
+        with h5py.File(inpath) as file:
+            u = file["/Trigger/u/0"][...]
+
         with h5py.File(triggerdir / "deltasigma=0.120_id=0_incc=11_element=0_istep=00.h5") as file:
-            system = QuasiStatic.System(file)
-            system.restore_quasistatic_step(file["Trigger"], 0)
-            i_n = np.copy(system.plastic.i).astype(np.int64)
-            system.restore_quasistatic_step(file["Trigger"], 1)
-            check = np.sum((system.plastic.i.astype(np.int64) - i_n)[:, 0])
+            trigger = QuasiStatic.System(file)
+
+            trigger.restore_quasistatic_step(file["Trigger"], 0)
+            trigger_i_n = np.copy(trigger.plastic.i)
+            self.assertTrue(np.allclose(u, trigger.u))
+
+            trigger.restore_quasistatic_step(file["Trigger"], 1)
 
         with path.Path(outdir):
 
@@ -335,19 +343,64 @@ class test_Trigger(unittest.TestCase):
 
             with h5py.File(outpath) as file:
                 root = file["Dynamics"]
-                system = QuasiStatic.System(file)
+                dynamics = QuasiStatic.System(file)
 
-                udof = np.zeros(system.vector.shape_dofval())
+                udof = np.zeros(dynamics.vector.shape_dofval())
                 udof[root["doflist"][...]] = root["u"]["0"][...]
-                system.u = system.vector.AsNode(udof)
-                i_n = np.copy(system.plastic.i).astype(np.int64)
+                dynamics.u = dynamics.vector.AsNode(udof)
+                i_n = np.copy(dynamics.plastic.i)
 
-                udof = np.zeros(system.vector.shape_dofval())
+                udof = np.zeros(dynamics.vector.shape_dofval())
                 udof[root["doflist"][...]] = root["u"][str(file["/Dynamics/inc"].size - 1)][...]
-                system.u = system.vector.AsNode(udof)
-                S = np.sum((system.plastic.i.astype(np.int64) - i_n)[:, 0])
+                dynamics.u = dynamics.vector.AsNode(udof)
 
-        self.assertEqual(S, check)
+        self.assertTrue(np.all(np.equal(trigger_i_n, i_n)))
+        self.assertTrue(np.all(np.equal(trigger.plastic.i, dynamics.plastic.i)))
+
+    def test_rerun_dynamics(self):
+        """
+        Rerun one trigger with Dynamics, check the macroscopic stress.
+        """
+
+        outdir = triggerdir / "Dynamics2"
+        commands = Trigger.cli_job_deltasigma(
+            ["--dev", "-f", infopath, "-d", 0.12, "-p", 2, "-o", outdir, "--nmax", 10]
+        )
+        triggername = Trigger.cli_run(["--dev"] + commands[-1].split(" ")[1:])
+        triggerpack = outdir / "tmp_Pack.h5"
+        triggerinfo = outdir / "tmp_Info.h5"
+        Trigger.cli_ensemblepack(["-f", "-o", triggerpack, triggername])
+        Trigger.cli_ensembleinfo(["--dev", "-f", "-o", triggerinfo, triggerpack])
+
+        with h5py.File(triggerinfo, "r") as file:
+            A = file["A"][0]
+
+        outname = outdir / "rerun_trigger.h5"
+        Dynamics.cli_run(["--dev", "-f", "--step", 1, "-o", outname, triggername])
+
+        with h5py.File(outname, "r") as file:
+            a = file["/Dynamics/A"][-1]
+            iiter = file["/Dynamics/inc"].size - 1
+            ustore = file[f"/Dynamics/u/{iiter:d}"][...]
+            doflist = file["/Dynamics/doflist"][...]
+
+        with h5py.File(triggername, "r") as file:
+            system = QuasiStatic.System(file)
+            u = file["/Trigger/u/1"][...]
+            system.u = u
+            Sig = system.plastic.Sig / system.sig0
+            Eps = system.plastic.Eps / system.eps0
+            Epsp = system.plastic.epsp / system.eps0
+
+        udof = np.zeros(system.vector.shape_dofval())
+        udof[doflist] = ustore
+        system.u = system.vector.AsNode(udof)
+
+        self.assertTrue(np.allclose(Sig, system.plastic.Sig / system.sig0))
+        self.assertTrue(np.allclose(Eps, system.plastic.Eps / system.eps0))
+        self.assertTrue(np.allclose(Epsp, system.plastic.epsp / system.eps0))
+
+        self.assertEqual(A, a)
 
     def test_rerun_eventmap(self):
         """
@@ -373,6 +426,95 @@ class test_Trigger(unittest.TestCase):
             S = file["S"][...]
 
         self.assertEqual(np.sum(S), check)
+
+
+class test_Dynamics(unittest.TestCase):
+    def test_elements_at_height(self):
+        """
+        Identify elements at a certain height above the weak layer.
+        """
+
+        N = 3**3
+        mesh = GooseFEM.Mesh.Quad4.FineLayer(N, N)
+
+        conn = mesh.conn()
+        coor = mesh.coor()
+        e = mesh.elementsMiddleLayer()
+
+        assert np.all(np.equal(Dynamics.elements_at_height(coor, conn, 0), e))
+        assert np.all(np.equal(Dynamics.elements_at_height(coor, conn, 1), e + N))
+        assert np.all(np.equal(Dynamics.elements_at_height(coor, conn, 2), e + N * 2))
+
+    def test_partial_storage(self):
+        """
+        Store the displacement filed only for a selection of elements.
+        """
+
+        with h5py.File(infopath, "r") as file:
+            A = file[f"/full/{idname}/A"][...]
+            N = file["/normalisation/N"][...]
+
+        step = np.argwhere(A == N).ravel()[-1]
+
+        with h5py.File(simpath, "r") as file:
+            system = QuasiStatic.System(file)
+            u = file[f"/QuasiStatic/u/{step:d}"][...]
+
+        plastic = system.plastic_elem
+        system.u = u
+        Sig = system.Sig() / system.sig0
+        Sig_p = Sig[plastic, ...]
+
+        vector = system.vector
+        partial = tools.PartialDisplacement(
+            conn=system.conn,
+            dofs=system.dofs,
+            element_list=plastic,
+        )
+        dofstore = partial.dof_is_stored()
+        doflist = partial.dof_list()
+
+        ustore = vector.AsDofs(u)[dofstore]
+        udof = np.zeros(vector.shape_dofval())
+        udof[doflist] = ustore
+        system.u = vector.AsNode(udof)
+
+        self.assertFalse(np.allclose(Sig, system.Sig() / system.sig0))
+        self.assertTrue(np.allclose(Sig_p, system.Sig()[plastic, ...] / system.sig0))
+
+    def test_AlignedAverage(self):
+
+        N = 10
+        nip = 4
+        elem = np.arange(N)
+        nitem = 10
+        V = np.random.random((N, nip, 2, 2))
+        D = np.random.random((nitem, N + 1, N, nip, 2, 2))
+        M = np.random.random((nitem, N + 1, N)) < 0.5
+
+        av = Dynamics.AlignedAverage(shape=[N + 1, N, 2, 2], elements=elem, dV=V)
+        check_00 = [enstat.static(shape=[N]) for i in range(N + 1)]
+        check_01 = [enstat.static(shape=[N]) for i in range(N + 1)]
+        check_10 = [enstat.static(shape=[N]) for i in range(N + 1)]
+        check_11 = [enstat.static(shape=[N]) for i in range(N + 1)]
+
+        for i in range(nitem):
+            for a in range(N + 1):
+                av.add_subsample(i, D[i, a, ...], roll=0, broken=~M[i, a, ...])
+                d = np.average(D[i, a, ...], weights=V, axis=1)
+                check_00[i].add_sample(d[..., 0, 0], mask=M[i, a, ...])
+                check_01[i].add_sample(d[..., 0, 1], mask=M[i, a, ...])
+                check_10[i].add_sample(d[..., 1, 0], mask=M[i, a, ...])
+                check_11[i].add_sample(d[..., 1, 1], mask=M[i, a, ...])
+
+        res = np.empty([N + 1, N, 2, 2])
+        for a in range(N + 1):
+            res[a, :, 0, 0] = check_00[a].mean()
+            res[a, :, 0, 1] = check_01[a].mean()
+            res[a, :, 1, 0] = check_10[a].mean()
+            res[a, :, 1, 1] = check_11[a].mean()
+
+        self.assertTrue(np.allclose(av.mean(), res, equal_nan=True))
 
 
 if __name__ == "__main__":
